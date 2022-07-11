f70746213d804c837dc0b71dc2b97b28d5c7187b
jenkins <labautistap@.liverpool.com.mx>
2022-07-11 11:02:52 -0500

    Archivo Java 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main (String [] args) {
-        System.out.println("Hola Mundo desde Git");
+        String x = "Hola Mundo desde Git";
+        System.out.println(x);
     }
 }
